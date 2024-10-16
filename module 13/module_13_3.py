ff8b88613e8f5b82800c07fbf6cb79f3807d9dd8
Nosach-l <qwert_987@mail.ru>
2024-10-16 10:54:39 +0600

    Update module_13_3.py

--- module 13/module_13_3.py
+++ module 13/module_13_3.py
@@ -11,13 +11,13 @@ dp = Dispatcher(bot, storage=MemoryStorage())
 @dp.message_handler(commands=['start'])
 async def start(message):
     print('Привет! Я бот помогающий твоему здоровью.')
-    await message.reply('Привет! Я бот помогающий твоему здоровью.')
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
 
 
 @dp.message_handler()
 async def all_message(message):
     print('Введите команду /start, чтобы начать общение.')
-    await message.reply('Введите команду /start, чтобы начать общение.')
+    await message.answer('Введите команду /start, чтобы начать общение.')
 
 
 if __name__ == '__main__':
